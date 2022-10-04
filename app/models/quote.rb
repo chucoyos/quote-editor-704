c7a4aae2b30fecb3954eb4fe5cd88d09e2ada66b
chucoyos <facturasmanzanillo@hotmail.com>
2022-10-03 22:26:54 -0500

    broadcast quotes to index page and order by id

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,10 @@
 class Quote < ApplicationRecord
+  
   validates :name, presence: true
+  
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  
+  # order quotes by id desc
+  scope :ordered, -> { order(id: :desc) }
+
 end
