bf94f2b728cd12c73b5763c60937b0570cd53b00
chucoyos <facturasmanzanillo@hotmail.com>
2022-10-10 18:53:20 -0500

    secure broadcasts by creating a different signed-stream-name

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,7 +4,7 @@ class Quote < ApplicationRecord
   
   validates :name, presence: true
   
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes" ] }, inserts_by: :prepend
   
   # order quotes by id desc
   scope :ordered, -> { order(id: :desc) }
